26008f3c746252401ba043abf06f4bedb5193fce
Viktor Kyssa <viktor.kyssa@cation.ee>
2021-02-01 09:48:41 +0200

    ImageInput for choosing photo from gallery or from camera and Deleting functionality

---
+++ app/components/ImageInput.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Image, TouchableWithoutFeedback, Alert } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+
+import colors from '../config/colors';
+
+function ImageInput({ imageUri, onChangeImage }) {
+    const requestPermission = async () => {
+        const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (!granted) 
+           alert('You need to enable permission to access the library.');
+     };
+
+     const deleteImage = () => {
+        Alert.alert('Delete', 'Are you sure you want to delete this image?', [
+            { text: 'Yes', onPress: () => onChangeImage(null) },
+            { text: 'No' }
+        ]);
+     };    
+
+    const selectImage = async () => {
+        try {
+           const result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.Images,
+              allowsEditing: true,
+              aspect: [4, 7],
+              quality: 0.5
+           });
+  
+           if (!result.cancelled) {
+              onChangeImage(result.uri);
+           }                    
+        } catch (error) {
+           console.log('Error reading an image', error);
+        }
+     };
+
+     const takePhoto = async () => {
+        try {
+           const result = await ImagePicker.launchCameraAsync({
+              allowsEditing: true
+           });
+  
+           if (!result.cancelled) {
+              onChangeImage(result.uri);
+           }
+        } catch (error) {
+           console.log('Error making a photo', error);
+        }
+     };
+
+     useEffect(() => {
+        requestPermission();
+     }, []);
+
+   return (
+        <View style={styles.container}>
+            { !imageUri && <MaterialCommunityIcons onPress={takePhoto} color={colors.medium} name='camera' size={40} /> }
+            { !imageUri && <MaterialCommunityIcons onPress={selectImage} color={colors.medium} name='image-area' size={40} /> }
+            { imageUri && (
+                <TouchableWithoutFeedback onPress={deleteImage}>
+                    <Image source={{ uri: imageUri }} style={styles.image} />
+                </TouchableWithoutFeedback>
+            )}
+        </View>
+   );
+}
+
+const styles = StyleSheet.create({
+   container: {
+        backgroundColor: colors.light,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+        width: 100,
+        overflow: 'hidden'
+   },
+   image: {
+       width: '100%',
+       height: '100%'
+   }
+});
+
+export default ImageInput;
--- app/components/Test.js
+++ app/components/Test.js
@@ -5,58 +5,17 @@ import * as Permissions from 'expo-permissions';
 
 import Screen from '../components/Screen';
 import AppButton from './Button/AppButton';
+import ImageInput from './ImageInput';
 
 function Test(props) {
    const [image, setImage] = useState(null);
 
-   const requestPermission = async () => {
-      const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (!granted) 
-         alert('You need to enable permission to access the library.');
-   };
-
-   useEffect(() => {
-      requestPermission();
-   }, []);
-
-   const selectImage = async () => {
-      try {
-         const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 7],
-            quality: 1
-         });
-
-         if (!result.cancelled) {
-            setImage(result.uri);
-         }                    
-      } catch (error) {
-         console.log('Error reading an image', error);
-      }
-   };
-
-   const takePhoto = async () => {
-      try {
-         const result = await ImagePicker.launchCameraAsync({
-            allowsEditing: true
-         });
-
-         if (!result.cancelled) {
-            setImage(result.uri);
-         }
-      } catch (error) {
-         console.log('Error making a photo', error);
-      }
-   };
-
    return (
      <Screen style={styles.container}>
-        <View style={styles.imageBtns}>
-            <AppButton style={styles.imageBtn} title='Select Image' onPress={selectImage} />
-            <AppButton style={styles.imageBtn} title='Take Photo' onPress={takePhoto} />
-        </View>
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <ImageInput 
+            imageUri={image} 
+            onChangeImage={uri => setImage(uri)}
+        />
      </Screen>
    );
 }
